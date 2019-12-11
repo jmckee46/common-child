ef70cb97f225a2bf059c0354075cceca20e0b5c1
John McKee <jmckee3@mac.com>
2019-12-11 15:18:21 -0800

    working version using straight recursion

--- common-child.go
+++ common-child.go
@@ -1,3 +1,29 @@
 package main
 
+func commonChild(s1 string, s2 string) int32 {
+	s1Length := int32(len(s1))
+	s2Length := int32(len(s2))
+
+	return longestCommonSubstring(&s1, &s2, s1Length, s2Length)
+}
+
+func longestCommonSubstring(s1 *string, s2 *string, s1Length int32, s2Length int32) int32 {
+	if s1Length == 0 || s2Length == 0 {
+		return 0
+	}
+	if (*s1)[s1Length-1] == (*s2)[s2Length-1] {
+		return 1 + longestCommonSubstring(s1, s2, s1Length-1, s2Length-1)
+	}
+
+	return max(longestCommonSubstring(s1, s2, s1Length, s2Length-1), longestCommonSubstring(s1, s2, s1Length-1, s2Length))
+}
+
+func max(x int32, y int32) int32 {
+	if x > y {
+		return x
+	}
+
+	return y
+}
+
 func main() {}
--- spec-common-child_test.go
+++ spec-common-child_test.go
@@ -2,6 +2,52 @@ package main
 
 import "testing"
 
-func TestCommonChild(t *testing.T) {
+func TestCommonChild1(t *testing.T) {
+	s1 := "AGGTAB"
+	s2 := "GXTXAYB"
+	lcs := commonChild(s1, s2)
 
+	if lcs != 4 {
+		t.Errorf("got %d instead of 124", lcs)
+	}
+}
+
+func TestCommonChild2(t *testing.T) {
+	s1 := "HARRY"
+	s2 := "SALLY"
+	lcs := commonChild(s1, s2)
+
+	if lcs != 2 {
+		t.Errorf("got %d instead of 2", lcs)
+	}
+}
+
+func TestCommonChild3(t *testing.T) {
+	s1 := "AA"
+	s2 := "BB"
+	lcs := commonChild(s1, s2)
+
+	if lcs != 0 {
+		t.Errorf("got %d instead of 0", lcs)
+	}
+}
+
+func TestCommonChild4(t *testing.T) {
+	s1 := "SHINCHAN"
+	s2 := "NOHARAAA"
+	lcs := commonChild(s1, s2)
+
+	if lcs != 3 {
+		t.Errorf("got %d instead of 3", lcs)
+	}
+}
+
+func TestCommonChild5(t *testing.T) {
+	s1 := "ABCDEF"
+	s2 := "FBDAMN"
+	lcs := commonChild(s1, s2)
+
+	if lcs != 2 {
+		t.Errorf("got %d instead of 2", lcs)
+	}
 }
